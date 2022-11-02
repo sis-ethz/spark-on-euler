5bfe3905fedafef2bf2a52e534c166943053c88e
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2022-11-02 23:16:42 +0100

    Add scripts

--- get_spark.sh
+++ get_spark.sh
@@ -1,5 +1,8 @@
-mkdir spark_home
+mkdir -p spark_home
 cd spark_home
 
 wget https://dlcdn.apache.org/spark/spark-3.2.2/spark-3.2.2-bin-hadoop3.2.tgz
 tar -xvzf spark-3.2.2-bin-hadoop3.2.tgz 
+
+rm spark-3.2.2-bin-hadoop3.2.tgz
+cd ..
