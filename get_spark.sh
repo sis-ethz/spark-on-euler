81cedc739a15aa16d362557cc67a2dff92679a1f
OlgaOvcharenko <44943723+OlgaOvcharenko@users.noreply.github.com>
2022-11-30 21:01:41 +0100

    Update spark link

--- get_spark.sh
+++ get_spark.sh
@@ -3,8 +3,8 @@
 mkdir -p spark_home
 cd spark_home
 
-wget https://dlcdn.apache.org/spark/spark-3.2.2/spark-3.2.2-bin-hadoop3.2.tgz
-tar -xvzf spark-3.2.2-bin-hadoop3.2.tgz 
+wget https://dlcdn.apache.org/spark/spark-3.2.3/spark-3.2.3-bin-hadoop3.2.tgz
+tar -xvzf spark-3.2.3-bin-hadoop3.2.tgz 
 
-rm spark-3.2.2-bin-hadoop3.2.tgz
+rm spark-3.2.3-bin-hadoop3.2.tgz
 cd ..
